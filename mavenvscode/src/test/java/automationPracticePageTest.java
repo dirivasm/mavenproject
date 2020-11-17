b8b08aec43c6103e3c43aa8d5f8ed02f66c98d48
dirivasm <62437745+dirivasm@users.noreply.github.com>
2020-11-17 10:08:44 -0600

    applaudo-cr-comments
    Code comments - AS

--- mavenvscode/src/test/java/automationPracticePageTest.java
+++ mavenvscode/src/test/java/automationPracticePageTest.java
@@ -21,6 +21,7 @@ public class automationPracticePageTest {
     AutomationPracticePage automationPage;
     String page = "http://automationpractice.com/index.php?";
     
+    //CR: There should be a BaseTest so this setup method should be reusable across all Classes and avoid DRY anti-pattern
     @BeforeTest
     public void setup() {
         WebDriverManager.chromedriver().setup();
@@ -34,7 +35,7 @@ public class automationPracticePageTest {
     public void add_items_to_cart() {
         String textExpected = "There is 1 item in your cart";
         String item = "Blouse";
-        driver.get(this.page);        
+        driver.get(this.page);  //CR: This should be part of setup method       
         automationPage.addItemToCart(item);
         Assert.assertTrue(general.isTextPresent(textExpected));
     }
@@ -43,7 +44,7 @@ public class automationPracticePageTest {
     public void remove_item_from_cart() throws InterruptedException {
         String textExpected = "Your shopping cart is empty.";
         String item = "Blouse";
-        driver.get(this.page);        
+        driver.get(this.page); //CR: This should be part of setup method       
         automationPage.clickShoppingCart();
         automationPage.deleteItemFromCart(item);
         Assert.assertTrue(general.isTextPresent(textExpected));       
@@ -53,7 +54,7 @@ public class automationPracticePageTest {
     public void search_invalid_item() throws InterruptedException {
         String searchItem = "invalidItem";
         String textExpected = "0 results have been found.";
-        driver.get(this.page);   
+        driver.get(this.page);   //CR: This should be part of setup method       
         automationPage.searchItem(searchItem);
         Assert.assertTrue(general.isTextPresent(textExpected));
     }
@@ -61,7 +62,7 @@ public class automationPracticePageTest {
     @Test
     public void search_valid_item() {
         String searchItem = "Blouse";
-        driver.get(this.page);   
+        driver.get(this.page);  //CR: This should be part of setup method       
         automationPage.searchItem(searchItem);
         Assert.assertTrue(general.isTextDisplayed("//div[@class='product-container']//a[@class='product-name' and @title='"+searchItem+"']"));
     }
@@ -69,7 +70,7 @@ public class automationPracticePageTest {
     @Test
     public void check_store_information() {
         String[] urls = {"https://www.facebook.com/groups/525066904174158/", "https://twitter.com/seleniumfrmwrk", "https://www.youtube.com/channel/UCHl59sI3SRjQ-qPcTrgt0tA"};
-        driver.get(this.page);
+        driver.get(this.page); //CR: This should be part of setup method       
         general.scrollToBottomPage();
         String fbUrl = driver.findElement(By.xpath("//li[@class='facebook']/a")).getAttribute("href");
         String twUrl = driver.findElement(By.xpath("//li[@class='twitter']/a")).getAttribute("href");
